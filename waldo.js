d0a44c2f1b60bea17ee1b2a1b5660cb239f078f5
jenniferhsueh <jenhsueh89@hotmail.com>
2018-05-10 21:40:25 +0000

    refactor waldo

--- waldo.js
+++ waldo.js
@@ -1,11 +1,11 @@
 // The second argument/parameter is expected to be a function
-function findWaldo(arr, found) {
-    for (var i = 0; i < arr.length; i++) {
-      if (arr[i] === "Waldo") {
+function findWaldo(array, found) {
+    // for (var i = 0; i < arr.length; i++) {
+      array.forEach((currentValueName, i) => {
+      if (currentValueName === "Waldo") {
         found(i);   // execute callback
-    
       }
-    }
+    })
   }
   function actionWhenFound(found) {
     console.log("Found him at index " + found);
